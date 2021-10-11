7800766cf064605456fd82d01f11a944f22ccd97
imres <imreszabo23@freemail.hu>
2021-10-11 18:22:25 +0200

    Remove comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import {useStore} from "react-redux";
-//import chartdata from "./components/chartdata/chartdata"
-//import RechartDiv from "./components/rechart/rechart";
 import Maincontent from "./components/maincontent/maincontent";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from "react";
