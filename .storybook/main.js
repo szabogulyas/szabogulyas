8c12caedf3100b7eb99657697ae4c909e45fc293
imres <imreszabo23@freemail.hu>
2021-10-14 17:28:44 +0200

    Final commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,16 @@
 module.exports = {
+  "stories": [
+    "../src/App.stories.@(js|jsx|ts|tsx)",
+    "../src/components/maincontent/heading.stories.tsx",
+    "../src/components/rechart/rechart.stories.tsx"
+  ],
+  "components": [
+    "../src/components/maincontent/heading.stories.tsx",
+    "../src/components/rechart/rechart.stories.tsx"
+  ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ],
-  "components": [
-    "../src/**/maincontent/*.stories.mdx",
-    "../src/**/maincontent/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/chartdata/*.stories.@(js|jsx|ts|tsx)",
-    "../src/**/rechart/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "mainapp": [
-    "../src/App.stories.@(js|jsx|ts|tsx)"
   ],
 }
